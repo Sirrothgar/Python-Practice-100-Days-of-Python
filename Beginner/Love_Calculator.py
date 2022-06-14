d534d66c7e15cbf4bfc92802fac3073c6c002ec5
Sirrothgar <57143302+Sirrothgar@users.noreply.github.com>
2022-06-14 14:53:10 -0400

    Update Love_Calculator.py

--- Beginner/Love_Calculator.py
+++ Beginner/Love_Calculator.py
@@ -15,19 +15,28 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-lower_case_name1 = name1.lower()
-lower_case_name2 = name2.lower()
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
 
-true = lower_case_name1.count("t") + lower_case_name1.count("r") + lower_case_name1.count("u") + lower_case_name1.count(
-    "e")
-love = lower_case_name2.count("l") + lower_case_name2.count("o") + lower_case_name2.count("v") + lower_case_name2.count(
-    "e")
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
 
 love_score = int(f"{true}{love}")
 
 if (love_score > 90) or (love_score < 10):
-    print(f"Your score is {love_score}, you go together like coke and mentos.")
+  print(f"Your score is {love_score}, you go together like coke and mentos.")
 elif (love_score > 40) and (love_score < 50):
-    print(f"Your score is {love_score}, you are alright together.")
+  print(f"Your score is {love_score}, you are alright together.")
 else:
-    print(f"Your score is {love_score}")
+  print(f"Your score is {love_score}")
