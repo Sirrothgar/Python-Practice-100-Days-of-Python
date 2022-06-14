63c39c0d4bf3c40fff9938374794f223d5f01e97
Sirrothgar <57143302+Sirrothgar@users.noreply.github.com>
2022-06-14 15:48:08 -0400

    Update Create_adventure.py

--- Beginner/Create_adventure.py
+++ Beginner/Create_adventure.py
@@ -25,24 +25,24 @@ print("Your mission is to find the treasure.")
 # Write your code below this line 👇
 road = input("You travel down the raod and come to a crossroad. Where do you want to go? Type 'left' or 'right' ").lower()
 
-if road == "right":
-    print("You didn't look good enough and fell into a bottomless pit. Game over.")
-elif road == "left":
-    print(f"The road continues down to a lake where you see a island in the middle.")
-
-    lake = input("Do you wait for a boat or swim across? Type 'Swim' or 'Wait'").lower()
-    if lake == "swim":
-        print("You forgot you don't know how to swim and drown. Game over.")
-    elif lake == "wait":
-        print(f"A boat falls out of the sky into the water to let you across.")
-
-        door = input("When you get to the island, there is a house with 3 doors. One red, one yellow, one blue. Which color do you choose?").lower()
-        if door == "red":
+if road == "left":
+    lake = input("The road continues down to a lake where you see a island in the middle. Do you wait for a boat or swim across? Type 'Swim' or 'Wait'").lower()
+    if lake == "wait":
+        door = input("A boat falls out of the sky into the water to let you across.When you get to the island, "
+                     "there is a house with 3 doors. One red, one yellow, one blue. Which color do you choose?").lower()
+        if door == "yellow":
+            print("You choose the right door and are safe. Congratulations, you won!")
+        elif door == "red":
             print("Fire shoots out of the door and you melt to death. Game over.")
         elif door == "blue":
-            print(
-                "A tidal wave comes through the door and pushes you into the lake where you drown because you can't swim. Game over.")
-        elif door == "yellow":
-            print("You choose the right door and are safe. Congratulations, you won!")
+            print("A tidal wave comes through the door and pushes you into the lake where you drown because you can't swim. Game over.")
         else:
             print("There is no door with that color, you're trying to cheat and I don't like cheaters so game over!")
+    elif lake == "swim":
+        print("You forgot you don't know how to swim and drown. Game over.")
+    else:
+        print("That wasn't an option, I'm sorry but game over.")
+elif road == "right":
+    print("You didn't look good enough and fell into a bottomless pit. Game over.")
+else:
+    print("You don't have a delorean to go in any other direction. Game over")
